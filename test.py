38aab766d91f938e53624fe5ef0ce29d76b87023
Chaojie1 <chaojiew2@nycstudents.net>
2025-04-23 09:25:11 -0400

    cat

--- test.py
+++ test.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    foxes = []
-    for _ in range(10):
-        response = requests.get("https://randomfox.ca/floof/").json()
-        foxes.append({'imageurl': response['image']})
-    return render_template("home.html", foxes=foxes)
+    cats = []
+    response = requests.get("https://api.thecatapi.com/v1/images/search?limit=10").json()
+    print(response)
+    for i in response:
+        cats.append({'imageurl': i['url']})
+        print(i)
+    return render_template("home.html", cats=cats)
+@app.route("/cat/<string:id>")
+def cat(id):
+    response = requests.get(f"https://api.thecatapi.com/v1/images/{id}")
+    data = response.json()
+    cats = [{'imageurl': data['url']}]
+    return render_template("home.html", cats=cats)
 if __name__ == '__main__':
     app.run(debug=True)
