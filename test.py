039b55b1be5377c4dfc51144d67d8fcdae108f47
Chaojie1 <chaojiew2@nycstudents.net>
2025-05-02 09:24:00 -0400

    i go brr

--- test.py
+++ test.py
@@ -5,23 +5,30 @@ app = Flask(__name__)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 }
+
+
 @app.route("/")
 def index():
-    streamers = []
     response = requests.get("https://api.chess.com/pub/streamers",headers=headers)
-    print(response)
-    if response.status_code != 200:
-        return f"API error: {response.status_code}"
-
     data = response.json()
-
+    streamers = {}
     for i in data["streamers"]:
-        streamer = {'avatar': i['avatar'], 'user': i['username'],'live':None}
+        streamers[i['username']] = {'avatar': i['avatar'], 'user': i['username'],'live':None}
         if i["is_live"] == True:
-            streamer['live'] = "Live"
+             streamers[i['username']]['live'] = "Live"
         else:
-            streamer['live'] = "Offline"
-        streamers.append(streamer)
+             streamers[i['username']]['live'] = "Offline"
     return render_template("home.html", streamers=streamers)
+@app.route("/streamer/<id>")
+def stre(id):
+    response = requests.get("https://api.chess.com/pub/streamers",headers=headers)
+    data = response.json()
+    streamer = None
+    for i in data["streamers"]:
+        if i["username"] == id:
+            streamer = i
+            break
+    print(streamer)
+    return render_template("specific.html", streamer=streamer)
 if __name__ == '__main__':
     app.run(debug=True)
